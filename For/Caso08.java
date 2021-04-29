127a262a17ed355313a08310a9ebb27c201f4faf
Alexson-Felipe <alexfelipesilveira@gmail.com>
2021-04-28 21:09:22 -0300

    Caso 08 e 10 resolvido

--- For/Caso08.java
+++ For/Caso08.java
@@ -1,10 +1,24 @@
+import javax.swing.JOptionPane;
 
 public class Caso08 {
 
+	static int imp;
+
 	public static void main(String[] args) {
-		
-		
 
+		// imp = Integer.parseInt(JOptionPane.showInputDialog(Di));
+
+		for (int i = 0; i <= 500; i++) {
+
+			if (i % 2 == 1) {
+
+				imp += i;
+
+				System.out.println(imp);
+			}
+
+		}
+
+		JOptionPane.showMessageDialog(null, "A soma de todos os numeros impares, seria: " + imp);
 	}
-
 }
--- For/Caso10.java
+++ For/Caso10.java
@@ -2,16 +2,16 @@ import javax.swing.JOptionPane;
 
 public class Caso10 {
 	
-	static int tab;
+	static int tab, tab1;
 
 	public static void main(String[] args) {
 		
 		tab = Integer.parseInt(JOptionPane.showInputDialog("Insira um numero"));
 		
-		for(int i = 1; i <= 10; i++) {
+		for(int i = 1; i <= 5; i++) {
 			
-			
-			
+			tab1 = tab * i;
+			JOptionPane.showMessageDialog(null, tab + " x " + i + " = " + tab1);
 		}
 		
 
