a1a5d3de4d8a73f792d59d7e332a601d510e90ee
Alexson Felipe <alexfelipesilveira@gmail.com>
2021-06-22 23:14:25 -0300

    Matriz

--- Matrizes/Ativ08d.java
+++ Matrizes/Ativ08d.java
@@ -3,29 +3,29 @@ import javax.swing.JOptionPane;
 /**
  * 
  * Desenvolva um programa que controle o carrinho de compras de um supermercado.
- * O aplicativo deve permitir o cadastro de até 10 produtos. O programa deve
- * permitir incluir as seguintes informações para cada produto.
+ * O aplicativo deve permitir o cadastro de atÃ© 10 produtos. O programa deve
+ * permitir incluir as seguintes informaÃ§Ãµes para cada produto.
  * 
- * ● Código 
- * ● Descrição 
- * ● Quantidade 
- * ● Valor Unitário 
- * ● Subtotal
+ * Código
+ * Descrição
+ * Quantidade 
+ * Valor Unitário 
+ * Subtotal
  * 
- * O menu principal do sistema deve conter as seguintes opções:
+ * O menu principal do sistema deve conter as seguintes opÃ§Ãµes:
  *
  * 1 - Adicionar produto ao carrinho 
  * 2 - Remover produto por código 
  * 
- * ● Deve ser realizada uma busca no programa pelo código informado pelo usuário e caso o mesmo seja encontrado, deve ser atribuído null nos atributos do carrinho. 
+ * â—� Deve ser realizada uma busca no programa pelo cÃ³digo informado pelo usuÃ¡rio e caso o mesmo seja encontrado, deve ser atribuÃ­do null nos atributos do carrinho. 
  * 
  * 3 - Ver carrinho
  * 
- * ● Listar todos os produtos, quantidade comprada, subtotal e total 
+ * â—� Listar todos os produtos, quantidade comprada, subtotal e total 
  * 
  * 4 - Ir para checkout 
  * 
- * ● Mostrar toda a lista dos produtos comprados e o total da venda e depois apresentar a mensagem “”Compra realizada com Sucesso” e fechar o programa.
+ * â—� Mostrar toda a lista dos produtos comprados e o total da venda e depois apresentar a mensagem â€œâ€�Compra realizada com Sucessoâ€� e fechar o programa.
  * 
  */
 public class Ativ08d {
@@ -43,7 +43,8 @@ public class Ativ08d {
 		do {
 			
 			menu = Integer.parseInt(JOptionPane.showInputDialog("[1] - Adicionar produto ao carrinho\n"
-					+ "[2] - Remover produto por código\n" + "[3] - Ver carrinho\n" + "[4] - Ir para checkout\n"));
+					+ "[2] - Remover produto por código\n" + "[3] - Ver carrinho\n" + "[4] ?"
+							+ "- Ir para checkout\n"));
 			
 			switch (menu) {
 			case 1:
@@ -57,9 +58,9 @@ public class Ativ08d {
 
 					}
 					produtos[i][0] = JOptionPane.showInputDialog("Código " + i );
-					///produtos[i][1] = JOptionPane.showInputDialog("Descrição");
+					///produtos[i][1] = JOptionPane.showInputDialog("DescriÃ§Ã£o");
 					//produtos[i][2] = JOptionPane.showInputDialog("Quantidade");
-					//produtos[i][3] = JOptionPane.showInputDialog("Valor unitário");
+					//produtos[i][3] = JOptionPane.showInputDialog("Valor unitario");
 					//produtos[i][4] = JOptionPane.showInputDialog("Subtotal");
 				// else {
 						
