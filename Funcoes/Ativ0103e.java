5e072ca59715608935f26e6dd19bead87b11c5a4
Alexson-Felipe <alexfelipesilveira@gmail.com>
2021-06-30 19:26:41 -0300

    Funções casos resolvidos

--- Funcoes/Ativ0103e.java
+++ Funcoes/Ativ0103e.java
@@ -1,32 +1,54 @@
 import javax.swing.JOptionPane;
 
-/**
- * 
- * Faça um programa para um time de futebol, onde eles precisam organizar onde
- * cada pessoa vai sentar no jogo. O programa deve solicitar as seguintes
- * informações: nome, tipo de sócio(vip ou normal), time (casa ou visitante) e
- * idade. Após inserir os dados, o programa deve mostrar onde é o acento da
- * pessoa de acordo com essa tabela.
- * 
- * #tabela no PDF
- *
- * Você deve criar uma função que receba os parâmetros sócio, time, idade e
- * retornar onde essa pessoa deve sentar. Depois mostre o nome e o local onde a
- * pessoa deve sentar.
- * 
- * 
- */
 public class Ativ0103e {
 
-	static String nome;
-	static char tipoSocio, time;
-	static int idade;
-	
-	public static void main(String[] args) {
-		
-		nome = JOptionPane.showInputDialog("Digite o nome");
+	static String[] cidades;
 
-		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		cidades = new String[3];
+
+		for (int i = 0; i < cidades.length; i++) {
+
+			cidades[i] = JOptionPane.showInputDialog("Digite o "
+					+ "nome da cidade").toLowerCase();
+		}
+
+		contaVogaisConsoantes(cidades);
+
+	}
+
+	private static void contaVogaisConsoantes(String[] cidades2) {
+		// TODO Auto-generated method stub
+
+		char[] vetorCidade;
+		int contVogal = 0;
+		for (int i = 0; i < cidades2.length; i++) {
+
+			vetorCidade = cidades2[i].toCharArray();
+
+			for (int j = 0; j < vetorCidade.length; j++) {
+
+				switch (vetorCidade[j]) {
+				case 'a':
+				case 'e':
+				case 'i':
+				case 'o':
+				case 'u':
+					contVogal++;
+					break;
+				default:
+					break;
+				}
+
+			}
+
+			System.out.println(cidades2[i] + " tem " + contVogal + " vogais. \n");
+			contVogal = 0;
+
+		}
+
 	}
 
 }
--- Funcoes/Ativ04e.java
+++ Funcoes/Ativ04e.java
@@ -21,16 +21,24 @@
 public class Ativ04e {
 
 	static String lista;
-	static String[] itensLista;
+	static String[] itensLista, nomesCidades;
 
 	public static void main(String[] args) {
-		lista = "Pessoa 1-Tubarao/SC; \r\n" + "Pessoa 2-Florianopolis/SC; \r\n" + "Pessoa 3-Sao Paulo/SP; \r\n"
-				+ "Pessoa 4-Osasco/SP; \r\n" + "Pessoa 5-Porto Alegre/RS; \r\n" + "Pessoa 6-Rio de Janeiro/RJ; \r\n"
-				+ "Pessoa 7-Sao Paulo/SP \r\n" + "Pessoa 8-Florianopolis/SC; \r\n" + "Pessoa 9-Rio de Janeiro/RJ; \r\n"
-				+ "Pessoa 10-Tubarao/SC;";
+		// TODO Auto-generated method stub
+
+		lista = "Pessoa 1-Tubarão/SC;\n" + "Pessoa 2-Florianópolis/SC;\n" + "Pessoa 3-São Paulo/SP;\n"
+				+ "Pessoa 4-Osasco/SP;\n" + "Pessoa 5-Porto Alegre/RS;\n" + "Pessoa 6- Rio de Janeiro/RJ;\n"
+				+ "Pessoa 7-São Paulo/SP\n" + "Pessoa 8-Florianópolis/SC;\n" + "Pessoa 9- Rio de Janeiro/RJ;\n"
+				+ "Pessoa 10-Tubarão/SC\n" + "";
 
 		itensLista = lista.split(";");
 
-	}
+		for (int i = 0; i < itensLista.length; i++) {
 
+			nomesCidades = itensLista[i].split("-");
+
+			System.out.println("Nome: " + nomesCidades[0] + ". " + "Mora em: " + nomesCidades[1]);
+		}
+
+	}
 }
