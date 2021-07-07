43d9fd5d63942357b99cbf289228818a96f3e4e6
Alexson-Felipe <alexfelipesilveira@gmail.com>
2021-07-06 22:20:07 -0300

    Registro

---
+++ Registro/Caso02g.java
@@ -0,0 +1,54 @@
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ * Desenvolva um programa que armazene em um registro as provas de um aluno,
+ * sendo eles: data de realização e nota. Crie também outro registro, só que
+ * armazenando os dados do aluno, sendo eles: nome, idade, primeira prova,
+ * segunda prova e terceira prova. Ao final apresente a média simples do aluno.
+ * Uma função deve ser utilizada para o calculo da média
+ *
+ */
+public class Caso02g {
+
+	static double CalcularMedia;
+
+	static class ProvasAluno {
+		String dataRealizacao;
+		double nota;
+
+	}
+
+	static class ArmazenaDados {
+		String nome;
+		ProvasAluno primeiraProva, segundaProva, terceiraProva;
+
+		int idade;
+
+	}
+
+	public static void main(String[] args) {
+
+		ProvasAluno prova = new ProvasAluno();
+		ArmazenaDados dados = new ArmazenaDados();
+
+		prova.dataRealizacao = JOptionPane.showInputDialog("Digite a data da realização da prova");
+
+		dados.nome = JOptionPane.showInputDialog("Nome do aluno");
+		dados.idade = Integer.parseInt(JOptionPane.showInputDialog("Idade do aluno"));
+//Rever isso -- tá errado
+		dados.primeiraProva.nota = Double.parseDouble(JOptionPane.showInputDialog("Primeira nota"));
+		dados.segundaProva.nota = Double.parseDouble(JOptionPane.showInputDialog("Segunda nota"));
+		dados.terceiraProva.nota = Double.parseDouble(JOptionPane.showInputDialog("Terceira nota"));
+
+		CalcularMedia = calculaMedia(dados.primeiraProva.nota, dados.segundaProva.nota, dados.terceiraProva.nota);
+
+		System.out.println(CalcularMedia);
+	}
+
+	private static double calculaMedia(double nota, double nota2, double nota3) {
+		// TODO Auto-generated method stub
+		return ((nota + nota2 + nota3) / 3);
+	}
+
+}
